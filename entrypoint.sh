fee16b838eb16986283ccca29662686e619e85da
alejandrojnm <alejandrojnm@gmail.com>
2023-03-21 11:26:28 +0000

    Update the dockerfile
    Signed-off-by: alejandrojnm <alejandrojnm@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 
+#  Downlaod the civo cli
+curl -sL https://civo.com/get | sh
+
+
 echo ">>> Adding the civo api key" >> $GITHUB_OUTPUT
 
 civo apikey save action $1
 civo apikey use action
 
-echo ">>> Successfully logged into civo" >> $GITHUB_OUTPUT
+echo ">>> Successfully logged into civo" >> $GITHUB_OUTPUT 
