06344d1187bb1f695b7e3aff02968820e5c89480
alejandrojnm <alejandrojnm@gmail.com>
2023-03-21 12:15:15 +0000

    Test to update the path
    Signed-off-by: alejandrojnm <alejandrojnm@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 echo "logs<<EOF" >> $GITHUB_OUTPUT
 
 curl -sL https://civo.com/get | sh
+
+echo "/usr/local/bin" >> $GITHUB_PATH
+
 version=$(civo version)
 
 echo "Civo CLI version: $version" >> $GITHUB_OUTPUT
